5b8a89839205374efb979a2439a9dfe80fe75871
TinuPrajapati <tinup2580@gmail.com>
2024-11-17 17:51:06 +0530

    change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,9 @@
 import { Outlet } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
 
 export default function App() {
   return (
     <>
       <Outlet />
-      <ToastContainer/>
     </>
   );
 }
