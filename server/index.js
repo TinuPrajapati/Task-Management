3821bf34d0bd94bf5c32db787484cbcf9618f00b
TinuPrajapati <tinup2580@gmail.com>
2024-11-19 00:26:35 +0530

    change

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "https://task-management-tinu.vercel.app",
+    origin: process.env.frontend_url,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
